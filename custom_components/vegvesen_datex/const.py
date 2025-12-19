6072c11fb4a5a7eb2106bef0bf5e5409c814896d
furnes <vidarfurnes@gmail.com>
2025-12-19 20:42:08 +0100

    Update const.py

--- custom_components/vegvesen_datex/const.py
+++ custom_components/vegvesen_datex/const.py
@@ -4,12 +4,21 @@ CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
 CONF_QUERY = "query"
 CONF_SCAN_INTERVAL = "scan_interval"
+CONF_SITE_ID = "site_id"
+CONF_SITE_NAME = "site_name"
 
 DEFAULT_QUERY = "Måløybrua"
 DEFAULT_SCAN_INTERVAL = 60
 
 SITUATION_URL_DEFAULT = (
     "https://datex-server-get-v3-1.atlas.vegvesen.no/datexapi/GetSituation/pullsnapshotdata"
+)
+
+WEATHER_SITE_TABLE_URL_DEFAULT = (
+    "https://datex-server-get-v3-1.atlas.vegvesen.no/datexapi/GetMeasurementWeatherSiteTable/pullsnapshotdata"
+)
+MEASURED_WEATHER_URL_DEFAULT = (
+    "https://datex-server-get-v3-1.atlas.vegvesen.no/datexapi/GetMeasuredWeatherData/pullsnapshotdata"
 )
 
 ATTR_MESSAGE = "message"
